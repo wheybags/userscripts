f2969faaae956a979056f35f007d9ec6a10ff1ec
Tom Mason <wheybags@wheybags.com>
2024-11-20 17:59:18 +0100

    Create github_actions_scroll_down.js

---
+++ github_actions_scroll_down.js
@@ -0,0 +1,73 @@
+// ==UserScript==
+// @name        ACTUALLY SCROLL DOWN - github actions logs
+// @namespace   Violentmonkey Scripts
+// @match       https://github.com/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 20/11/2024, 16:49:06
+// ==/UserScript==
+
+
+
+let following = false;
+let interval = null;
+let button = null;
+
+
+function follow()
+{
+  following = true;
+  button.innerHTML = "Stop Following"
+  interval = setInterval(() =>
+  {
+    window.scrollBy(0, 99999);
+  }, 100);
+}
+
+function stopFollowing()
+{
+  following = false;
+  button.innerHTML = "Start Following";
+
+  if (interval !== null)
+  {
+    clearInterval(interval);
+    interval = null;
+  }
+}
+
+function deactivate()
+{
+  stopFollowing();
+  button.remove();
+  button = null;
+}
+
+function activate()
+{
+  button = document.createElement("button");
+  button.style.width = "150px";
+  button.style.height = "50px";
+  button.style.backgroundColor = "white";
+  button.style.position = "fixed";
+  button.style.left = "50vw";
+  button.style.top = "0";
+  button.style.color = "red";
+  button.innerHTML = "Start Follow";
+  button.onclick = () => { following ? stopFollowing() : follow(); };
+  document.body.appendChild(button);
+  follow();
+}
+
+setInterval(()=>
+{
+  const regex = /^https:\/\/github\.com\/[^\/]*\/[^\/]*\/actions\/runs\/[^\/]*\/job\/[^\/]*$/gm;
+  const matches = !!window.location.toString().match(regex);
+
+  if (button && !matches)
+    deactivate();
+  else if (!button && matches)
+    activate();
+}, 500)
+
